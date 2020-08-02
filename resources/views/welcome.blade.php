ee73adda7ee73038f643a9115197b002be9a5880
Ubuntu <ubuntu@ip-172-31-36-216.ap-northeast-1.compute.internal>
2020-08-02 11:07:07 +0000

    CRUD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
+                    {!! $tasks !!}
                 </div>
 
                 <div class="links">
